d94b05eed133ac5f87632e2d35fcae4bc7a9d5f9
Rapkowsky <robertrapkowski19@gmail.com>
2022-01-14 21:18:07 +0100

    API communication/  variables / img name changed

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ const photo = document.querySelector(".photo");
 const weather = document.querySelector(".weather");
 const temperature = document.querySelector(".temperature");
 const humidity = document.querySelector(".humidity");
+
+const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "&appid=13fe38d29dd8cc085ef827d782d636c5";
+const API_UNITS = "&units=metric";
+
+const getWeather = () => {
+	const city = input.value || "Mrągowo";
+	const URL = API_LINK + city + API_KEY + API_UNITS;
+
+	axios.get(URL).then((res) => {
+		console.log(res);
+		const temp = res.data.main.temp;
+		const hum = res.data.main.humidity;
+
+		cityName.textContent = res.data.name;
+		temperature.textContent = Math.floor(temp) + "°C";
+		humidity.textContent = hum + "%";
+	});
+};
+
+getWeather();
